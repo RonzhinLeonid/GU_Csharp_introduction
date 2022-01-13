3f63dfbdd81a787fc2df1783c9dd8dfb435fc622
Leonid <jiehuh87@yandex.ru>
2022-01-13 22:53:23 +0300

    Les5

---
+++ ConsoleApp1/Les5/Complex.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Les5
+{
+    public class Complex
+    {
+        double _re;
+        double _im;
+
+        public double Re
+        {
+            get
+            {
+                return _re;
+            }
+        }
+        public double Im
+        {
+            get 
+            { 
+                return _im;
+            }
+        }
+
+        public Complex(double re, double im)
+        {
+            _re = re;
+            _im = im;
+        }
+        public static Complex Sum(Complex x, Complex y)
+        {
+            return new Complex( 
+                x.Re + y.Re,
+                x.Im + y.Im);
+        }
+
+        public static Complex operator +(Complex x, Complex y)
+        {
+            return new Complex(
+                x.Re + y.Re,
+                x.Im + y.Im);
+        }
+
+        public static Complex Subtraction(Complex x, Complex y)
+        {
+            return new Complex(
+                x.Re - y.Re,
+                x.Im - y.Im);
+        }
+
+        public static Complex operator -(Complex x, Complex y)
+        {
+            return new Complex(
+                x.Re - y.Re,
+                x.Im - y.Im);
+        }
+
+        public static Complex Multi(Complex x, Complex y)
+        {
+            return new Complex(
+                x.Re * y.Re - x.Im * y.Im,
+                x.Im * y.Re + x.Re * y.Im);
+        }
+        public static Complex operator *(Complex x, Complex y)
+        {
+            return new Complex(
+                x.Re * y.Re - x.Im * y.Im,
+                x.Im * y.Re + x.Re * y.Im);
+        }
+        public static bool operator ==(Complex x, Complex y)
+        {
+            return !(x is null) && !(y is null) && x.Im == y.Im && x.Re == y.Re;
+        }
+        public static bool operator !=(Complex x, Complex y)
+        {
+            return !(x == y);
+        }
+        public override bool Equals(object? obj)
+        {
+            return obj is Complex complex && complex.Im == _im && complex.Re == _re;
+        }
+        protected bool Equals(Complex other)
+        {
+            return _im == other.Im && _re == other.Re;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Im, Re);
+        }
+
+        public override string ToString()
+        {
+            if (_im == 0) return $"{_re}";
+            else if (_re == 0) return $"{_im}i";
+            else return (_im < 0) ? $"{_re} - {-_im}i" : $"{_re} + {_im}i";
+        }
+
+    }
+}
---
+++ ConsoleApp1/Les5/RationalNumber.cs
@@ -0,0 +1,194 @@
+﻿using System;
+
+namespace Les5
+{
+    public class RationalNumber
+    {
+        int _numerator;
+        int _denominator;
+
+        /// <summary>
+        /// Числитель
+        /// </summary>
+        public int Numerator 
+        { 
+            get
+            {
+                return _numerator;
+            }
+        }
+
+        /// <summary>
+        /// Знаменатель
+        /// </summary>
+        public int Denominator 
+        { 
+            get
+            {
+                return _denominator;
+            }
+        }
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="numerator">Числитель</param>
+        /// <param name="denominator">Знаменатель</param>
+        public RationalNumber(int numerator, int denominator)
+        {
+            if (denominator == 0) throw new Exception("Деление числа на ноль не допускается");
+            _numerator = numerator;
+            _denominator = denominator;
+            //перенос минуса в числитель из знаменателя
+            if (_denominator < 0)
+            {
+                _numerator *= -1;
+                _denominator *= -1;
+            }
+            ReductionRationalNumber();
+        }
+
+        public static bool operator ==(RationalNumber r1, RationalNumber r2)
+        {
+            return !(r1 is null) && !(r2 is null) && r1.Numerator == r2.Numerator && r1.Denominator == r2.Denominator;
+        }
+
+        public static bool operator !=(RationalNumber r1, RationalNumber r2)
+        {
+            return !(r1 == r2);
+        }
+
+        public static bool operator >(RationalNumber r1, RationalNumber r2)
+        {
+            return r1.Numerator * r2.Denominator > r1.Denominator * r2.Numerator;
+        }
+
+        public static bool operator <(RationalNumber r1, RationalNumber r2)
+        {
+            return r1.Numerator * r2.Denominator < r1.Denominator * r2.Numerator;
+        }
+
+        public static bool operator >=(RationalNumber r1, RationalNumber r2)
+        {
+            return !(r1 < r2);
+        }
+
+        public static bool operator <=(RationalNumber r1, RationalNumber r2)
+        {
+            return !(r1 > r2);
+        }
+
+        public static RationalNumber operator +(RationalNumber r1, RationalNumber r2)
+        {
+            return new RationalNumber(r1.Numerator * r2.Denominator + r2.Numerator * r1.Denominator, r1.Denominator * r2.Denominator);
+        }
+
+        public static RationalNumber operator -(RationalNumber r1, RationalNumber r2)
+        {
+            return new RationalNumber(r1.Numerator * r2.Denominator - r2.Numerator * r1.Denominator, r1.Denominator * r2.Denominator);
+        }
+
+        public static RationalNumber operator -(RationalNumber r)
+        {
+            return new RationalNumber(-r.Numerator, r.Denominator);
+        }
+
+        public static RationalNumber operator ++(RationalNumber r)
+        {
+            return new RationalNumber(r.Numerator + r.Denominator, r.Denominator);
+        }
+
+        public static RationalNumber operator --(RationalNumber r)
+        {
+            return new RationalNumber(r.Numerator - r.Denominator, r.Denominator);
+        }
+
+        public static RationalNumber operator *(RationalNumber r1, RationalNumber r2)
+        {
+            return new RationalNumber(r1.Numerator * r2.Numerator, r1.Denominator * r2.Denominator);
+        }
+
+        public static RationalNumber operator /(RationalNumber r1, RationalNumber r2)
+        {
+            return new RationalNumber(r1.Numerator * r2.Denominator, r1.Denominator * r2.Numerator);
+        }
+
+        public static RationalNumber operator %(RationalNumber r1, RationalNumber r2)
+        {
+            return new RationalNumber((r1.Numerator * r2.Denominator) % (r1.Denominator * r2.Numerator), r1.Denominator * r2.Denominator);
+        }
+
+        public static implicit operator float(RationalNumber r)
+        {
+            return (float)r.Numerator / r.Denominator;
+        }
+
+        public static implicit operator int(RationalNumber r)
+        {
+            return r.Numerator / r.Denominator;
+        }
+
+        public static explicit operator RationalNumber(int numerator)
+        {
+            return new RationalNumber(numerator, 1);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is RationalNumber rationalNumber && rationalNumber.Numerator == _numerator && rationalNumber.Denominator == _denominator;
+        }
+
+        protected bool Equals(RationalNumber other)
+        {
+            return _numerator == other.Numerator && _denominator == other.Denominator;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Numerator, Denominator);
+        }
+
+        public override string ToString()
+        {
+            return $"{_numerator}/{_denominator}";
+        }
+
+        /// <summary>
+        /// Наибольший общий делитель
+        /// </summary>
+        /// <param name="numerator"> Числитель</param>
+        /// <param name="denominator">Знаменатель</param>
+        /// <returns>Наибольший общий делитель</returns>
+        private int Nod(int numerator, int denominator)
+        {
+            int temp = 0;
+
+            numerator = Math.Abs(numerator);
+            denominator = Math.Abs(denominator);
+
+            if (denominator > numerator)
+            {
+                temp = numerator;
+                numerator = denominator;
+                denominator = temp;
+            }
+            do
+            {
+                temp = numerator % denominator;
+                numerator = denominator;
+                denominator = temp;
+            } while (denominator != 0);
+
+            return numerator;
+        }
+        /// <summary>
+        /// Сокращение дроби 
+        /// </summary>
+        private void ReductionRationalNumber()
+        {
+            int nod = Nod(_numerator, _denominator);
+            _numerator = _numerator / nod;
+            _denominator = _denominator / nod;
+        }
+    }
+}
